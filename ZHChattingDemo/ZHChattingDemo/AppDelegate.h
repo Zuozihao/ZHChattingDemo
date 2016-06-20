dbfdb2a4a8e3f711347a260f8e27cbfec82981c8
Zuozihao <978111349@qq.com>
2016-06-20 16:40:02 +0800

    New Project

---
+++ ZHChattingDemo/ZHChattingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHChattingDemo
+//
+//  Created by 左梓豪 on 16/6/20.
+//  Copyright © 2016年 左梓豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHChattingDemo/ZHChattingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHChattingDemo
+//
+//  Created by 左梓豪 on 16/6/20.
+//  Copyright © 2016年 左梓豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
